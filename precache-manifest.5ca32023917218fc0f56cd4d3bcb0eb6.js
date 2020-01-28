c8d858041d0824f751036d1273aac500f74a3ae0
Francis Ng <francis-ng@outlook.com>
2020-01-28 20:42:58 +0900

    Updates

---
+++ precache-manifest.5ca32023917218fc0f56cd4d3bcb0eb6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70dd2ee18bda60ef1e369d1b55042736",
+    "url": "/fintracker-client/index.html"
+  },
+  {
+    "revision": "96c3090bd81f4ee190ce",
+    "url": "/fintracker-client/static/css/2.62c6fba7.chunk.css"
+  },
+  {
+    "revision": "83aca6729e89e1556f0f",
+    "url": "/fintracker-client/static/css/main.18930ee1.chunk.css"
+  },
+  {
+    "revision": "96c3090bd81f4ee190ce",
+    "url": "/fintracker-client/static/js/2.0c734c1c.chunk.js"
+  },
+  {
+    "revision": "0c08a48b164499df78c0607b14eb2c76",
+    "url": "/fintracker-client/static/js/2.0c734c1c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "83aca6729e89e1556f0f",
+    "url": "/fintracker-client/static/js/main.40340ce9.chunk.js"
+  },
+  {
+    "revision": "c647aadec1767a79f6d2",
+    "url": "/fintracker-client/static/js/runtime-main.493a3c2f.js"
+  }
+]);
